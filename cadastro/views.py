240402b9a85b248f49fa355c1f1606a4c9f17af5
cassiano <csiqueira10@gmail.com>
2025-05-17 19:56:31 -0300

    iniciando app de cadastro

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse 
+from .forms import ProfessorForm
+from .models import Professor
 
-# Create your views here.
+def home(request):
+    return HttpResponse('estou na home')
+
+def cadastro(request):
+    formulario = ProfessorForm(request.POST or None)
+    
+    if formulario .is_valid():
+        formulario.save()
+        return HttpResponse('professor cadastrado!') 
+    
+    return render(request, 'cadastro.html', {'form':formulario})
